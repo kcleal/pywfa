9c9a62259e611061e30599d12876522e4d231bed
kcleal <clealk@cardiff.ac.uk>
2022-03-17 15:45:34 +0000

    Added pyximport

--- pywfa/__init__.py
+++ pywfa/__init__.py
@@ -1,4 +1,5 @@
-# from __future__ import absolute_import
+import pyximport
+pyximport.install(reload_support=True)
 
 from pywfa.align import WavefrontAligner, clip_cigartuples, cigartuples_to_str, elide_mismatches_from_cigar
 
