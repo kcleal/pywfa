ea96cc7c4cab8b5b9f96ae7f1f15d1df704f8bc4
kcleal <clealk@cardiff.ac.uk>
2022-07-15 10:18:18 +0100

    Moved to v2.2 WFA2-lib

--- setup.py
+++ setup.py
@@ -8,33 +8,80 @@ from subprocess import run
 import os
 import glob
 import shutil
+from distutils import ccompiler
+import distutils.sysconfig
 
-extras = ["-Wno-sign-compare", "-Wno-unused-function", "-Wno-unused-result", '-Wno-ignored-qualifiers',
-          "-Wno-deprecated-declarations"]
 
-ext_modules = list()
+debug = False
+
+cy_options = {
+    'annotate': False,
+    'compiler_directives': {
+        'profile': debug,
+        'linetrace': debug,
+        'boundscheck': debug,
+        'wraparound': debug,
+        'nonecheck': debug,
+        'initializedcheck': debug,
+        'language_level': 3
+    }
+}
+
+cfg_vars = distutils.sysconfig.get_config_vars()
+for key, value in cfg_vars.items():
+    if type(value) == str:
+        cfg_vars[key] = value.replace("-Wstrict-prototypes", "")
+
+
+def has_flag(compiler, flagname):
+    """Return a boolean indicating whether a flag name is supported on
+    the specified compiler.
+    """
+    import tempfile
+    with tempfile.NamedTemporaryFile('w', suffix='.c') as f:
+        f.write('int main (int argc, char **argv) { return 0; }')
+        try:
+            compiler.compile([f.name], extra_postargs=[flagname])
+        except setuptools.distutils.errors.CompileError:
+            return False
+    return True
+
+
+def get_extra_args(flags):
+    compiler = ccompiler.new_compiler()
+    extra_compile_args = []
+    for f in flags:
+        if has_flag(compiler, f):
+            extra_compile_args.append(f)
+    return extra_compile_args
+
+
+extras = ["-Wno-unused-function", "-Wno-unused-result", '-Wno-ignored-qualifiers', "-Wno-deprecated-declarations"]
+extras_pywfa = get_extra_args(extras)
 
 root = os.path.abspath(os.path.dirname(__file__))
 wfa = os.path.join(root, "pywfa/WFA2_lib")
-
 libraries = [f"{wfa}/lib"]
 library_dirs = [f"{wfa}/lib"]
 include_dirs = [".", root, wfa, f"{wfa}/lib", f"{wfa}/utils", f"{wfa}/wavefront", f"{wfa}/bindings/cpp", f"{wfa}/system",
-                f"{root}/pywfa"]
+                f"{wfa}/alignment", f"{root}/pywfa"]
 
 print("Libs", libraries)
 print("Library dirs", library_dirs)
 print("Include dirs", include_dirs)
 
+ext_modules = list()
 ext_modules.append(Extension("pywfa.align",
                              ["pywfa/align.pyx"],
                              libraries=libraries,
                              library_dirs=library_dirs,
                              include_dirs=include_dirs,
-                             extra_compile_args=extras,
+                             extra_compile_args=extras_pywfa,
                              language="c",
                              extra_objects=[f"{wfa}/lib/libwfa.a"]
                              ))
+
+openmp_support = False
 
 
 class MyBuild(_build_ext):
@@ -42,8 +89,9 @@ class MyBuild(_build_ext):
     def build_extension(self, ext):
         # This is a hack to automate python setup.py build_ext --inplace during install
         # Note sure how robust this is
-        if self.debug:
-            ext.extra_compile_args.append("-O0")
+        global openmp_support
+        compiler = ccompiler.new_compiler()
+        openmp_support = has_flag(compiler, '-fopenmp')
 
         shared = glob.glob(f"{root}/build/lib*/pywfa/*.so") + glob.glob(f"{root}/build/lib*/pywfa/*.dll")
         _build_ext.build_extension(self, ext)
@@ -53,8 +101,13 @@ class MyBuild(_build_ext):
 class Build_ext_first(_install):
     def run(self):
         # Build WFA2-lib first
-        # run("cd pywfa/WFA2_lib; make BUILD_CPP=0 clean all; cd ../../", shell=True)
-        run("cd pywfa/WFA2_lib; make clean all; cd ../../", shell=True)
+        # run("cd pywfa/WFA2_lib; make clean all; cd ../../", shell=True)
+        global openmp_support
+        if openmp_support:
+            run("cd pywfa/WFA2_lib; make clean all BUILD_WFA_PARALLEL=1 BUILD_MINIMAL=1; cd ../../", shell=True)
+        else:
+            run("pwd", shell=True)
+            run("cd pywfa/WFA2_lib; make clean all BUILD_WFA_PARALLEL=0 BUILD_MINIMAL=1; cd ../../", shell=True)
         return setuptools.command.install.install.run(self)
 
 setup(
@@ -64,7 +117,7 @@ setup(
     url="https://github.com/kcleal/pywfa",
     description="Align sequences using WFA2-lib",
     license="MIT",
-    version='0.2.7',
+    version='0.3.0',
     python_requires='>=3.7',
     install_requires=[  # runtime requires
             'cython',
@@ -76,7 +129,7 @@ setup(
             'pysam', 'nose'
         ],
     packages=["pywfa", "pywfa/tests"],
-    ext_modules=cythonize(ext_modules),
+    ext_modules=cythonize(ext_modules, **cy_options),
     cmdclass={'install': Build_ext_first, 'build_ext': MyBuild},
     include_package_data=True,
     zip_safe=False,
