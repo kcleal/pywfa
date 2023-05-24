9452ff542da26787f65ac346db5f7c5ebdc87b62
Adam English <ACEnglish@gmail.com>
2023-05-24 12:42:02 -0400

    removing comments
    left intermediate, non-working lines commented out in the code

--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name="pywfa",
-      version="0.5.0-dev1",)
-      #data_files=[('.',["_custom_build.py"])],
-      #package_data = {'':["_custom_build.py"]},
-      #include_package_data=True)
+setup(name="pywfa", version="0.5.0-dev1")
